84e5801a5ca86c8840ad1e38262755a0bffcb23f
jm3d3l <32960752+jm3d3l@users.noreply.github.com>
2020-08-03 18:51:13 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [str, onChangeInput] = useInputNumberHook();
+  const [newRef, handleOnPaste] = IsCharHook();
+  const [onChangeInput] = useInputNumberHook();
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <input value={str} name="input" onChange={onChangeInput} />
+      <input
+        name="input"
+        onPaste={handleOnPaste}
+        refs={newRef}
+        onChange={onChangeInput}
+      />
     </div>
   );
 }
 
-const useInputNumberHook = (onChange = null, specialValue) => {
-  const numRef = React.useRef("");
-  const [str, setNum] = React.useState("");
-  React.useEffect(() => {
-    setNum(specialValue);
-  }, [specialValue]);
-
+const useInputNumberHook = () => {
+  const numRef = useRef();
+  // useEffect(() => {
+  //   setNum(specialValue);
+  // }, [specialValue]);
+  // console.log(hasChar);
   const onChangeInput = ({ target: { name, value } }) => {
+    console.log("onchange", value);
     // @#$@#$@#$
     // daksdk23123!@!##!@#
     // sdffl234234
     // @#$@$$!@#@#!#1231313
-    let str = "";
-    let regex = /[^a-zA-Z0-9 ]/g;
-    let letterNumber = /\w+/;
-    if (value.match(letterNumber) instanceof Array) {
-      console.log(
-        "instance of ara",
-        value.match(letterNumber) instanceof Array
-      );
-      str = value.replace(regex, "");
-      setNum(str);
-    } else {
-      setNum(state => state);
-    }
-    let e = {
-      target: {}
-    };
+    // if (!hasChar) {
+    //   let str = "";
+    //   let regex = /[^a-zA-Z0-9 ]/g;
 
-    e.target.name = name;
-    e.target.value = str;
-    // onChange(e)
+    //   str = value.replace(regex, "");
+    //   numRef.current = str;
+    // }
   };
-  return [str, onChangeInput];
+
+  return [onChangeInput];
+};
+
+const IsCharHook = () => {
+  const numRef = useRef();
+  const [hasChar, setHasChar] = useState(false);
+  let letterNumber = /\w+/;
+  const handleOnPaste = ({ clipboardData }) => {
+    let newVal = clipboardData.getData("Text");
+    console.log("onPaste", newVal);
+    // if (newVal.match(letterNumber) instanceof Array) {
+    //   setHasChar(false);
+    //   numRef.current = value;
+    // } else {
+    //   setHasChar(true);
+    //   numRef.current = newVal;
+    // }
+  };
+
+  return [hasChar, handleOnPaste];
 };
